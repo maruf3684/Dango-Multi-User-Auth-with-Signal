1b6c9dfb6f22ebc52bc0abc50b7621ad63663cb4
Md Maruf Hasan Munna <maruf3684@gmail.com>
2022-01-02 05:24:35 +0600

    Update admin.py

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -39,3 +39,27 @@ admin.site.register(Account,AccountAdmin)
 
 admin .site.register(Teacher)
 admin .site.register(Student)
+
+
+
+
+
+#django default vabe session authentication use kore sei table dakhte hole nicher moto code likhte hobe
+
+#lets see sessions
+# from django.contrib.sessions.models import Session
+# admin.site.register(Session)
+
+#for more detail with expire date
+
+import pprint
+from django.contrib.sessions.models import Session
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return pprint.pformat(obj.get_decoded()).replace('\n', '<br>\n')
+    _session_data.allow_tags=True
+    list_display = ['session_key', '_session_data', 'expire_date']
+    readonly_fields = ['_session_data']
+    exclude = ['session_data']
+    date_hierarchy='expire_date'
+admin.site.register(Session, SessionAdmin)
